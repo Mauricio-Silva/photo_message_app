ddd0fd73f25a6e01aefaa5eec33e93b877e0057f
Mauricio@Aspire5 <sir.silvabmauricio@gmail.com>
2022-12-21 16:55:22 -0400

    multiple screens

--- frontend/main.py
+++ frontend/main.py
@@ -1,6 +1,7 @@
 # cSpell:disable
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 import requests
 from PIL import Image
 import io
@@ -11,37 +12,54 @@ MY_KEY = ""
 BASE_URL = ""
 
 
-class MainPage(BoxLayout):
+class StartScreen(Screen):
+    pass
+
+class CameraScreen(Screen):
+    pass
+
+class ChatScreen(Screen):
+    pass
+
+       
+
+class MainApp(App):
+    def build(self):
+        # self.file = Builder.load_file("main.kv")
+        self.screen_managment = ScreenManager()
+        self.screens = [StartScreen(name="main-page"), CameraScreen(name="camera"), ChatScreen(name="chat0xx")]
+        self.screen_managment.add_widget(self.screens[0])
+        self.screen_managment.add_widget(self.screens[1])
+        self.screen_managment.add_widget(self.screens[2])
+        self.screen_managment.current = "main-page"
+        return self.screen_managment
+
+
+    # sm.switch_to(screens[1], direction='right')
+
 
 
     def start_new_conversation(self):
-        global MY_KEY, BASE_URL
-        BASE_URL = self.ids["url"].text
-        request = requests.get(f"http://{BASE_URL}/new-key", headers=HEADERS)
-        assert request.status_code == 200
-        MY_KEY = dict(request.json()).get("key")
-        print(MY_KEY)
+        # global MY_KEY, BASE_URL
+        # BASE_URL = self.ids["url"].text
+        # request = requests.get(f"http://{BASE_URL}/new-key", headers=HEADERS)
+        # assert request.status_code == 200
+        # MY_KEY = dict(request.json()).get("key")
+        # print(f"Key: {MY_KEY}")
+        self.screen_managment.switch_to(self.screens[2], direction="left")
 
-
+    
     def enter_into_conversation(self):
-        global MY_KEY, BASE_URL
-        key = self.ids["user2_key"].text
-        request = requests.get(f"http://{BASE_URL}/connect-key/{key}", headers=HEADERS)
-        assert request.status_code == 200
-        MY_KEY = dict(request.json()).get("key")
+        # global MY_KEY, BASE_URL
+        # key = self.ids["user2_key"].text
+        # request = requests.get(f"http://{BASE_URL}/connect-key/{key}", headers=HEADERS)
+        # assert request.status_code == 200
+        # MY_KEY = dict(request.json()).get("key")
+        self.screen_managment.switch_to(self.screens[2], direction="left")
 
 
-    def send_image(self):
-        global MY_KEY
-        print(MY_KEY)
-        camera = self.ids['camera']
-        camera.export_to_png(f"images/{MY_KEY}.jpg")
-        data = {"key": MY_KEY}
-        with open(f"images/{MY_KEY}.jpg","rb") as file:
-            image = file.read()
-        files = {"image": image}
-        request = requests.post(f"http://{BASE_URL}/send-image", data=data, files=files)
-        assert request.status_code == 200
+    def back_to_start(self):
+        self.screen_managment.switch_to(self.screens[0], direction="right")
 
 
     def get_image(self):
@@ -53,14 +71,29 @@ class MainPage(BoxLayout):
         self.ids["async_image"].source = f"images/{MY_KEY}.jpg"
 
 
-        
-        
+    def take_picture(self):
+        self.screen_managment.switch_to(self.screens[1], direction="left")
 
 
+    def send_image(self):
+        # global MY_KEY
 
-class MainApp(App):
-    def build(self):
-        return MainPage()
+        camera = self.screen_managment.get_screen("camera").ids["camera"]
+        # camera = self.get_screen("camera").ids['camera']
+
+        # camera.export_to_png(f"images/{MY_KEY}.jpg")
+        camera.export_to_png(f"images/001.jpg")
+        # data = {"key": MY_KEY}
+        # with open(f"images/{MY_KEY}.jpg","rb") as file:
+        #     image = file.read()
+        # files = {"image": image}
+        # request = requests.post(f"http://{BASE_URL}/send-image", data=data, files=files)
+        # assert request.status_code == 200
+
+        self.screen_managment.get_screen("camera")
+        # self.screen_managment.get_screen("chat").ids["async_image"].source = f"images/001.jpg"
+        self.screen_managment.switch_to(self.screens[2], direction="right")
+
 
 
 
